2f2c52fba0ff6b3cdd00e2c7f5b0ba26b0d67254
koltyhin <130803420+CoidMeal@users.noreply.github.com>
2026-04-14 02:16:11 +0300

    Refactor error handling in stress data saving

--- app.py
+++ app.py
@@ -71,18 +71,18 @@ with tab1:
         st.subheader(f"Стресс: {int(stress)}")
 
         if st.button("💾 Сохранить"):
-           try:
-               supabase.table("stress").insert({
-               "user": user,
-               "time": str(datetime.datetime.now()),
-               "stress": float(stress),
-               "type": "test"
-               }).execute()
+            try:
+                supabase.table("stress").insert({
+                "user": user,
+                "time": str(datetime.datetime.now()),
+                "stress": float(stress),
+                "type": "test"
+                }).execute()
 
-               st.success("Сохранено")
+            st.success("Сохранено")
 
-            except Exception as e:
-                st.error(e)
+             except Exception as e:
+                 st.error(e)
     # ---------- SAN ----------
     with sub2:
         st.header("САН")
